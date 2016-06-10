7b4033b9ef8ef11e4cd6538a7414cbe05e227224
Kyle Luck <luck.kyle@gmail.com>
2016-06-10 13:20:26 -0400

    dedenting lines

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 getWeather();
-function startTime() {
-    var today = new Date();
-    var h = today.getHours() % 12;
-    var m = today.getMinutes();
-    var s = today.getSeconds();
-    m = checkTime(m);
-    s = checkTime(s);
-    document.getElementById('clock').innerHTML =
-    h + ":" + m + ":" + s;
-    var t = setTimeout(startTime, 1000);
 
+function startTime() {
+  var today = new Date();
+  var h = today.getHours() % 12;
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById('clock').innerHTML =
+  h + ":" + m + ":" + s;
+  var t = setTimeout(startTime, 1000);
 }
+
 function checkTime(i) {
   if (i < 10) {
     i = "0" + i;
   }
-    return i;
+  return i;
 }
 
 function getWeather() {
